4696b55446a749e8f8deced4e0e4f218f271c6fd
samer <s.allaham@ltuc.com>
2021-06-03 12:19:19 +0300

    pushing class10 demo

---
+++ class-10/demo/app.js
@@ -0,0 +1,65 @@
+'use strict';
+
+
+// let car;
+// console.log(car);
+
+
+// let globalVar='hello form the global sope'
+
+// if (true) {
+//     let localVar='hello from inside a code block';
+
+//     var localVar2='made with var keyword';
+//     console.log(globalVar);
+// }
+
+// console.log(localVar2);
+
+// global scope
+// function test() {
+//     // local scope 
+//     var funcLocalVar='hello from inside the function';
+// }
+
+// console.log(funcLocalVar);
+
+
+
+// console.log(userName);
+
+// let delete='delete';
+
+// console.log(delete);
+// let -samer=''
+
+
+// Refrence error:
+// when you try to use a variable that doesnt exist
+
+
+// RangeError:
+// a number outside of the allowed rane of values
+
+// let arr=[90,80];
+// arr.length=90**99;
+// console.log(arr);
+
+// Syntax Error:
+
+// let man='man';
+// let car x='bmw'
+
+
+
+// type Error
+// let num=123;
+// num.push(5);
+// num.toUpperCase();
+
+
+// console.log(String);
+
+// console.log(Number);
+// console.log(Array);
+// console.log(Object);
